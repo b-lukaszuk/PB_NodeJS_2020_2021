e25e938f4a1e06a6807324f095239c55bdc5f404
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-01-21 23:10:32 +0100

    zad 5, kosmetyka, poprawki na zgodnosc z wymaganiami

--- zaliczenie_sem1/z5/z5.js
+++ zaliczenie_sem1/z5/z5.js
@@ -1,4 +1,7 @@
-// import modules
+// import node modules
+const ps = require("process");
+
+// import my modules
 const userModule = require("./user.js");
 const userAlbums = require("./albums.js");
 const albumPhotos = require("./albumPhotos.js");
@@ -8,7 +11,29 @@ const getUsrEmail = userModule.getUsrEmail;
 const getUsrAlbums = userAlbums.getUsrAlbums;
 const getAlbumPhotos = albumPhotos.getAlbumPhotos;
 
-const userId = 1;
+function correctOrDefault(maybeDigit, defaultDigit) {
+  const parsedDigit = parseInt(maybeDigit);
+  if (isNaN(parsedDigit) || parsedDigit === 0) {
+    return defaultDigit;
+  } else {
+    return parsedDigit;
+  }
+}
+
+const defDigit = 2;
+
+// argv[0] exec node-a, argv[1] sciezka tego pliku
+// w razie braku wartosc domyslna
+const userId = correctOrDefault(ps.argv[2], defDigit);
+const albumId = correctOrDefault(ps.argv[3], defDigit);
+
+console.log("searching for:");
+console.log(
+  `\tuser id (if incorrect input search for id = ${defDigit}): ${userId}`
+);
+console.log(
+  `\talbum id: (if incorrect input search for id = ${defDigit}): ${albumId}`
+);
 
 getUsrEmail(userId)
   .then((response) => {
@@ -19,8 +44,12 @@ getUsrEmail(userId)
 getUsrAlbums(userId)
   .then((response) => {
     console.log("No of albums: " + response.length);
-    getAlbumPhotos(response[0])
-      .then((response) => console.log("Album photos:\n" + response))
-      .catch((err) => console.log(err));
   })
   .catch((error) => console.log(error));
+
+getAlbumPhotos(albumId)
+  .then((response) => console.log("Album photos:\n" + response))
+  .catch((err) => console.log(err));
+
+// przyklad wywolania funkcji
+// > node z5.js {usrId} {albumId}
