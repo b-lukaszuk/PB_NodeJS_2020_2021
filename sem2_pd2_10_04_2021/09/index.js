4f15234787aaced960feed24bc67d290e0b94191
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-24 08:01:19 +0200

    dodano rozw zad 9

--- sem2_pd2_10_04_2021/09/index.js
+++ sem2_pd2_10_04_2021/09/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const port = 4700;
-const fs = require("fs");
-const path = require("path");
+
+function pauseXms(milisecs) {
+    let start = new Date();
+    while ((new Date() - start) <= milisecs) {
+        // nothing to do here, just wait
+    }
+}
+
+const myMid = (req, res, next) => {
+    let reqReceivedTime = new Date().toLocaleTimeString();
+    console.log("Request received: ", reqReceivedTime);
+    next();
+}
+
+app.use(myMid);
 
 app.get("/", (req, res) => {
     res.send("The main page");
+    pauseXms(1000);
+    let resSendTime = new Date().toLocaleTimeString();
+    console.log("Response send: ", resSendTime);
 })
 
 app.listen(port, () => {
