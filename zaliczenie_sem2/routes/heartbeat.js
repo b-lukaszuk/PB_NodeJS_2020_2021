ab9799acbb5325ea480e1b35fd51a13942ef1179
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-06-08 16:12:48 +0200

    usuniecie niepotrzebnej funkcji

--- zaliczenie_sem2/routes/heartbeat.js
+++ zaliczenie_sem2/routes/heartbeat.js
@@ -7,28 +7,12 @@ const router = express.Router();
 
 
 ///////////////////////////////////////////////////////////////////////////////
-//                                 functions                                 //
-///////////////////////////////////////////////////////////////////////////////
-/**
- * returns string, date and time
- * my locale is english-US, may not work with other locales
- * @returns {string} string in format "day month year hh:mm:ss"
- */
-function nowDateTime() {
-    let now = new Date().toUTCString();
-    let [garbage1, day, month, year, time, garbage2] = now.split(" ");
-    let result = `${day} ${month} ${year} ${time}`;
-    return result;
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
 //                                 api/routes                                //
 ///////////////////////////////////////////////////////////////////////////////
 // here / stands for
 // http://localhost:4700/heartbeat (see index.js where we use this route)
 router.get("/", (req, res) => {
-    res.send(nowDateTime());
+    res.send(new Date().toLocaleString());
 });
 
 module.exports = router;
